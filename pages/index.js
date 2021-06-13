fce586f2e9b5aeb071c04f8bdc5c76178560d683
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-06-13 18:28:38 -0500

    ⚰️ remove unnecessary useEffect in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import { useEffect } from 'react'
 import HeadObject from '../components/head'
 import Nav from '../components/nav';
 
 export default function Home() {
-  useEffect(() => {
-    console.log('Made by Sarthak Mohanty. All Rights Reserved. Want to hire me? https://srtk.me')
-  });
-
   return (
     <div className="dark:text-white dark:bg-black">
       <HeadObject>
